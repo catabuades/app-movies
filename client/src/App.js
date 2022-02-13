aeca96c65f71caf0213d7cb81abf1ef8f1336434
catabuades <catabuades@gmail.com>
2022-02-13 13:12:51 +0100

    Listing styled responsive, addTop bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import './App.scss';
+import './mixins.scss'
+import TopBar from './Components/TopBar/TopBar'
 import Listing from './Components/Listing/Listing'
 
 const arrayMovies = [
+  {
+    image: 'https://static.filmin.es/images/media/35488/1/poster_0_3_1000x1500.png',
+    title: 'Días del Cielo',
+    director: 'Terrence Malick',
+    categories: ['Drama', 'Romance'],
+    año: '1978',
+    rate: 7.8
+  },
+  {
+    image: 'https://static.filmin.es/images/media/17230/1/poster_0_3_1000x1500.png',
+    title: 'La Tortuga Roja',
+    director: 'Michael Dudok de Wit',
+    categories: ['Animación', 'Aventuras', 'Drama', 'Fantástico'],
+    año: '2016',
+    rate: 8.1
+  },
+  {
+    image: 'https://static.filmin.es/images/media/12027/1/poster_0_3_1000x1500.png',
+    title: 'La Novia Vestía de Negro',
+    director: 'François Truffaut',
+    categories: ['Clásicos', 'Thriller'],
+    año: 1967,
+    rate: 7.0
+  },
   {
     image: 'https://static.filmin.es/images/media/35488/1/poster_0_3_1000x1500.png',
     title: 'Días del Cielo',
@@ -32,6 +58,7 @@ const arrayMovies = [
 function App() {
   return (
     <div className="App">
+      <TopBar></TopBar>
       <Listing movies={arrayMovies}></Listing>
     </div>
   );
---
+++ client/src/Components/TopBar/TopBar.js
@@ -0,0 +1,11 @@
+import './base.scss';
+
+function TopBar({  }) {
+  return (
+    <div className="topBar">
+      <a href="" className="topBar__link">ACCOUNT</a>
+    </div>
+  );
+}
+
+export default TopBar;
