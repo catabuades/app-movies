6102cbc576497fd1eb51419a5657b47f4e6c6835
catabuades <catabuades@gmail.com>
2022-03-11 16:29:47 +0100

    fix: add new fetch to listing

--- client/src/Components/Listing/Listing.js
+++ client/src/Components/Listing/Listing.js
@@ -2,6 +2,7 @@ import './base.scss';
 import React, { useState, useEffect } from 'react'
 import Poster from '../Poster/Poster'
 import Sidebar from '../Sidebar/Sidebar'
+import { fetchMoviesByCategory } from '../../Services/filtersService' 
 
 function Listing({ movies }) {
   const [isOpen, setOpen] = useState(false);
@@ -12,30 +13,13 @@ function Listing({ movies }) {
     setOpen(!isOpen); 
   };
 
-  useEffect(() => {
-    setListingMovies(movies);
-  }, [movies]);
-
-  const handleSelectCategory = (event, category) => {
-    // Hacer deep copy de la array de productos para así poder modificarla
-    const moviesCopy = [...listingMovies]
-
-    category = event.value
-
-    console.log(category)
-
-    /*
-    function checkCategory(moviesCopy, category) {
-      console.log(moviesCopy)
-      console.log(category)
-      return moviesCopy.some(function(arrCategory) {
-        return category === arrCategory;
-      });
+  const handleSelectCategory = (category) => {
+    async function fetchMoviesFilterCategory() {
+      const data = await fetchMoviesByCategory(category)
+      setListingMovies(data)
     }
 
-    checkCategory(moviesCopy, category)
-    console.log(checkCategory(moviesCopy, category))
-    */
+    fetchMoviesFilterCategory(category)
   };
 
   return (
