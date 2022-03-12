eba359825a5bd5132caf81504d3510a7805f470f
catabuades <catabuades@gmail.com>
2022-03-12 11:41:22 +0100

    add new route

--- backend/app.js
+++ backend/app.js
@@ -55,7 +55,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', express.static('./public'));
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/movies', catalogRouter);
